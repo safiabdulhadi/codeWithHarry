adb49fbfe2e5ad89a0d8526de5071e2ec8fc5d4d
safiabdullhadi <safiabdulhadi1994@gmail.com>
2021-11-02 20:43:54 +0100

    i did start from console.log

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+console.time('Your code Took');
 console.log("Hello World!");
 console.log(4 + 45);
 console.log('hello console');
@@ -6,3 +7,7 @@ console.log([34, 2 ,3 ,1]);//This is Array
 
 console.log({harry: 'this',mark :35}); // JavaScript object 
 console.table ({harry : 'this', mark:45});
+//console.warn('This is  warning');
+console.timeEnd('Your code Took');
+console.error('This is an errror');
+/* This is a multiline coment */
