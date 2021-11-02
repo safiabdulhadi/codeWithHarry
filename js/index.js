c23593fce3881a11be7c932e4a94d1951a2f348f
safiabdullhadi <safiabdulhadi1994@gmail.com>
2021-11-02 20:29:17 +0100

    I start to learn js on youtube

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+console.log(4 + 45);
+console.log('hello console');
+console.log(true);
+console.log([34, 2 ,3 ,1]);//This is Array
+
+console.log({harry: 'this',mark :35}); // JavaScript object 
+console.table ({harry : 'this', mark:45});
